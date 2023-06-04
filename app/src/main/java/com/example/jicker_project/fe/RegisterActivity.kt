a47ef6d18e40ff13df79439731c08c5be3ad4e62
Lover1975 <drjoenh1983@gmail.com>
2023-06-04 01:38:50 -0700

    Post request fro register.

--- app/src/main/java/com/example/jicker_project/fe/RegisterActivity.kt
+++ app/src/main/java/com/example/jicker_project/fe/RegisterActivity.kt
@@ -3,10 +3,14 @@ package com.example.jicker_project.fe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.example.jicker_project.R
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import java.io.IOException
 import kotlin.math.log
 
 class RegisterActivity : AppCompatActivity() {
@@ -27,6 +31,18 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun post(url: String, json: String, callback: Callback) {
+        val json1 = "application/json; charset=utf-8".toMediaTypeOrNull()
+        val client = OkHttpClient()
+        val body = RequestBody.create(json1, json)
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .build()
+        client.newCall(request).enqueue(callback)
+    }
+
     private fun initUIActions() {
         val signUpBtn = findViewById<Button>(R.id.btnSignUp)
         val loginPageButton = findViewById<TextView>(R.id.textView_login_page)
@@ -43,6 +59,32 @@ class RegisterActivity : AppCompatActivity() {
                 openDialog(1)
             } else if (!passOneText.text.toString().equals(passTwoText.text.toString())) {
                 openDialog(2)
+            } else {
+                println("here")
+                val json = """
+                            {
+                              "username": "${usernameText.text}",
+                              "password": "${passOneText.text}"
+                            }
+                            """.trimIndent()
+                println(json)
+                val url = "http://10.0.2.2:8000/api/accounts/register/"
+                post(url, json, object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        println("Response Code: ${response.code}")
+                        if (!response.isSuccessful) {
+                            println("Unexpected code $response")
+                        } else {
+                            println("are we here or above?")
+                            println(response.body?.string())
+                            callLogin()
+                        }
+                    }
+                })
             }
         }
         loginPageButton.setOnClickListener{
@@ -50,5 +92,10 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intentOne)
         }
 
+    }
+
+    private fun callLogin(){
+        val intentOne = Intent(this, LoginActivity::class.java)
+        startActivity(intentOne)
     }
 }
